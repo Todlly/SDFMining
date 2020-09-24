7c14cf68a2186d1ffc5d7e1d27b922d4b3257ee9
Todly <furrykunstler@gmail.com>
2020-09-24 22:58:52 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace serv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket server = new Socket(SocketType.Stream, ProtocolType.Tcp);
+            server.Bind(new IPEndPoint(IPAddress.Any, 7000));
+
+            server.Listen(5);
+
+            while (true)
+            {
+                Socket client = server.Accept();
+
+                string stream = "";
+                while (true)
+                {
+                    byte[] buff = new byte[8];
+                    client.Receive(buff);
+                    string s = Encoding.ASCII.GetString(buff);
+
+                    string[] st = s.Split('\0');
+
+                    if (s.IndexOf('\n') == -1)
+                    {
+                        foreach (string i in st)
+                            stream += i;
+                        continue;
+                    }
+
+                    Console.Write(stream);
+                }
+            }
+        }
+    }
+}
