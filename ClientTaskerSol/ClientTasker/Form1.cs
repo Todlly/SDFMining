cb8f1db0f1e3c6a66ef5036dc7f1f050a47564e0
Todly <furrykunstler@gmail.com>
2020-10-29 00:22:09 +0300

    working with tasker

--- ClientTaskerSol/ClientTasker/Form1.cs
+++ ClientTaskerSol/ClientTasker/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +13,23 @@ namespace ClientTasker
 {
     public partial class Form1 : Form
     {
+        static IPEndPoint endpoint = new IPEndPoint(3232235523, 7000);
+
+        Recruiter recruiter;
+
         public Form1()
         {
             InitializeComponent();
+            Timer timer = new Timer();
+            recruiter = new Recruiter(endpoint);
+            timer.Interval = 1000;
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+           recruiter.GiveJob(0, new int[] { 2, 3 });
         }
     }
 }
---
+++ ClientTaskerSol/ClientTasker/Recruiter.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientTasker
+{
+    public class Recruiter
+    {
+        private Socket ServerSocket { get; }
+        private EndPoint Server { get; }
+        public List<int> WorkersIPs { get; private set; }
+
+        public Recruiter(EndPoint endpoint)
+        {
+            ServerSocket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+
+            Server = endpoint;
+
+            try
+            {
+                ServerSocket.Connect("192.168.0.3", 7000);
+            } catch(Exception e)
+            {
+                
+            }
+        }
+
+        public int[] GiveJob(int workerID, int[] data)
+        {
+            string dataString = "";
+            for (int i = 0; i < data.Length; i++)
+            {
+                dataString += data[i];
+                if (i != data.Length - 1)
+                    dataString += '|';
+            }
+            string command = "job " + workerID + " " + dataString + "\r\n";
+
+            ServerSocket.Send(Encoding.ASCII.GetBytes(command));
+
+            byte[] answer = new byte[1024];
+            //int ind = ServerSocket.Receive(answer);
+            //if (ind > 0)
+            //{
+            //    string[] strNums = Encoding.ASCII.GetString(answer).Substring(0, ind).Split('|');
+            //    int[] output = new int[strNums.Length];
+
+            //    for (int i = 0; i < output.Length; i++)
+            //        output[i] = Convert.ToInt32(strNums[i]);
+
+            //    return output;
+            //}
+
+            return new int[0];
+        }
+    }
+}
